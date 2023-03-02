c1427abe520bca7aeb31249e70bed1f1ced353aa
Indra <wuppie55@gmail.com>
2023-03-03 00:26:20 +0100

    Add referrers query

--- src/Repositories/VisitRepository.php
+++ src/Repositories/VisitRepository.php
@@ -17,4 +17,21 @@ class VisitRepository
     {
         $this->database->insert("visits", $visit);
     }
+
+    public function getReferrers(int $site = 0)
+    {
+        $query = "SELECT `referrer_host` AS `referrer`, count(*) as `count` FROM `visits` WHERE `referrer_host` IS NOT NULL";
+        $params = [];
+        
+        if ($site > 0)
+        {
+            $query .= " AND `site_id` = ?";
+            
+            array_push($params, $site);
+        }
+
+        $query .= " GROUP BY `referrer_host` ORDER BY `count` DESC";
+
+        return $this->database->getAll($query, ...$params);
+    }
 }
