1c0dbcfa818d3b47205c1a22a8213560d8026c80
Indra <wuppie55@gmail.com>
2023-03-07 20:23:04 +0100

    Display list of API keys and add addKey method

--- src/Controllers/PageController.php
+++ src/Controllers/PageController.php
@@ -6,6 +6,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Shika\Helpers\Session;
 use Shika\Helpers\Twig;
+use Shika\Repositories\ApiKeyRepository;
 use Shika\Repositories\SiteRepository;
 use Shika\Repositories\UserRepository;
 
@@ -16,14 +17,16 @@ class PageController
 
     private SiteRepository $sites;
     private UserRepository $users;
+    private ApiKeyRepository $keys;
 
-    public function __construct(Twig $twig, Session $session, SiteRepository $sites, UserRepository $users)
+    public function __construct(Twig $twig, Session $session, SiteRepository $sites, UserRepository $users, ApiKeyRepository $keys)
     {
         $this->twig = $twig;
         $this->session = $session;
         
         $this->sites = $sites;
         $this->users = $users;
+        $this->keys = $keys;
     }
 
     public function index(Request $request, Response $response)
@@ -43,6 +46,6 @@ class PageController
 
     public function keys(Request $request, Response $response)
     {
-        return $this->twig->render($response, "dashboard/keys.html.twig");
+        return $this->twig->render($response, "dashboard/keys.html.twig", [ "keys" => $this->keys->getKeys() ]);
     }
 }
--- src/Repositories/ApiKeyRepository.php
+++ src/Repositories/ApiKeyRepository.php
@@ -17,4 +17,21 @@ class ApiKeyRepository
     {
         return $this->database->get("SELECT * FROM `user_api_keys` WHERE key = ?", $key);
     }
+
+    public function getKeys()
+    {
+        return $this->database->getAll("SELECT user_api_keys.*, users.username FROM user_api_keys INNER JOIN users ON user_api_keys.user_id = users.id");
+    }
+
+    public function addKey(string $label, string $key, int $user)
+    {
+        $data = [
+            "user_id" => $user,
+            "label" => $label,
+            "key" => $key,
+            "created_at" => gmdate("Y-m-d H:i:s")
+        ];
+
+        $this->database->insert("user_api_keys", $data);
+    }
 }
