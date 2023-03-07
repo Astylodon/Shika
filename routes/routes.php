2704e8fe8acf909a526ba4b3af35e49ffbfc0cf6
Indra <wuppie55@gmail.com>
2023-03-07 01:22:55 +0100

    Add some empty dashboard pages

--- routes/routes.php
+++ routes/routes.php
@@ -1,16 +1,26 @@
 <?php
 
 use Shika\Controllers\AnalyticsController;
-use Shika\Controllers\HomeController;
 use Shika\Controllers\LoginController;
+use Shika\Controllers\PageController;
 use Shika\Middleware\AuthMiddleware;
 use Slim\App;
+use Slim\Routing\RouteCollectorProxy;
 
 return function (App $app) {
+    // send endpoint
     $app->post("/analytics/send", [AnalyticsController::class, "send"]);
 
+    // login
     $app->get("/login", [LoginController::class, "show"]);
     $app->post("/login", [LoginController::class, "login"]);
 
-    $app->get("/", [HomeController::class, "index"])->add(AuthMiddleware::class);
+    // dashboard pages
+    $app->group("", function (RouteCollectorProxy $group) {
+        $group->get("/", [PageController::class, "index"]);
+        $group->get("/sites", [PageController::class, "sites"]);
+        $group->get("/users", [PageController::class, "users"]);
+        $group->get("/users/api/keys", [PageController::class, "keys"]);
+    })
+    ->add(AuthMiddleware::class);
 };
---
+++ src/Controllers/PageController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Shika\Controllers;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Shika\Helpers\Session;
+use Shika\Helpers\Twig;
+use Shika\Repositories\SiteRepository;
+use Shika\Repositories\UserRepository;
+
+class PageController
+{
+    private Twig $twig;
+    private Session $session;
+
+    private SiteRepository $sites;
+    private UserRepository $users;
+
+    public function __construct(Twig $twig, Session $session, SiteRepository $sites, UserRepository $users)
+    {
+        $this->twig = $twig;
+        $this->session = $session;
+        
+        $this->sites = $sites;
+        $this->users = $users;
+    }
+
+    public function index(Request $request, Response $response)
+    {
+        return $this->twig->render($response, "dashboard/index.html.twig", [ "username" => $this->session->get("user.name") ]);
+    }
+
+    public function sites(Request $request, Response $response)
+    {
+        return $this->twig->render($response, "dashboard/sites.html.twig", [ "sites" => $this->sites->getSites() ]);
+    }
+
+    public function users(Request $request, Response $response)
+    {
+        return $this->twig->render($response, "dashboard/users.html.twig", [ "users" => $this->users->getUsers() ]);
+    }
+
+    public function keys(Request $request, Response $response)
+    {
+        return $this->twig->render($response, "dashboard/keys.html.twig");
+    }
+}
