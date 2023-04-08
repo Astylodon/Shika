4dd22ba653ac8b6ad85d3b1d307f72e3d8a2b58f
Indra <wuppie55@gmail.com>
2023-04-08 03:30:43 +0200

    Send analytics after page has loaded

--- scripts/src/shika.js
+++ scripts/src/shika.js
@@ -1,6 +1,8 @@
-function shika_send() {
-    const url = document.currentScript.dataset.url;
-    const siteKey = document.currentScript.dataset.siteKey; // data-site-key
+const currentScript = document.currentScript
+
+function send() {
+    const url = currentScript.dataset.url; // data-url
+    const siteKey = currentScript.dataset.siteKey; // data-site-key
 
     if (!url || !siteKey) {
         console.error("The Shika script tag is missing a data-url or data-site-key attribute, make sure you copied the full code for your site.");
@@ -13,11 +15,11 @@ function shika_send() {
         href: location.href,
         siteKey
     };
-    const headers = {
-        type: 'application/json',
-    };
+
+    const headers = { type: 'application/json' };
+
     const blob = new Blob([JSON.stringify(body)], headers);
     navigator.sendBeacon(url, blob);
 }
 
-shika_send();
+window.addEventListener("load", send)
