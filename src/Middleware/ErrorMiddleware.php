ec5db42a035a4a2f07219a9ba533ba29c82c080d
Indra <wuppie55@gmail.com>
2023-04-08 03:00:29 +0200

    Make ErrorMiddleware write a JSON response

--- src/Middleware/ErrorMiddleware.php
+++ src/Middleware/ErrorMiddleware.php
@@ -4,11 +4,14 @@ namespace Shika\Middleware;
 
 use Psr\Http\Message\RequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Shika\Helpers\JsonResponse;
 use Slim\Exception\HttpException;
 use Slim\Psr7\Response;
 
 class ErrorMiddleware
 {
+    use JsonResponse;
+
     public function __invoke(Request $request, RequestHandler $handler)
     {
         try
@@ -18,8 +21,18 @@ class ErrorMiddleware
         catch (HttpException $e)
         {
             $response = new Response();
-            $response->getBody()->write($e->getMessage());
             
+            // write a JSON response for API endpoints
+            if (substr($request->getUri()->getPath(), 0, 5) == "/api/")
+            {
+                $body = [ "code" => $e->getCode(), "message" => $e->getMessage() ];
+
+                return $this->json($response, $body)->withStatus($e->getCode());
+            }
+            
+            // write a regular response
+            $response->getBody()->write($e->getMessage());
+
             return $response->withStatus($e->getCode());
         }
     }
