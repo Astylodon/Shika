87c950511d2f79aba190a587f0f9148d0762d5c1
Xwilarg <xwilarg@protonmail.com>
2025-05-11 18:44:47 +0200

    Clean script

--- setup.sh
+++ setup.sh
@@ -2,20 +2,6 @@
 set -e
 
 # User config
-read -p "Database name [shika]: " dataname
-if [ -z $dataname ]
-then
-    dataname=shika
-fi
-read -p "Database username [admin]: " username
-if [ -z $username ]
-then
-    username=admin
-fi
-while [ -z $password ]
-do
-    read -p "Database password: " password
-done
 while true; do
     read -p "Choose a database 1: MySQL, 2: SQLite [2] " database
     case $database in
@@ -26,34 +12,38 @@ while true; do
     esac
 done
 
-echo ""
-while true; do
-    read -p read -p "Your selection: $dataname $username $password $database, y to continue [y] " confirm
-    case $confirm in
-        [yY]* ) break;;
-        "" ) break;;
-        * ) echo "Invalid choice";;
-    esac
-done
-
 # Create .env file
 path=$(dirname $(realpath $0))
 echo ""
 echo "Creating environment at $path"
-if [ $database == "1" ]
+if [ $database == "1" ] # MySQL
 then
+    read -p "Database name [shika]: " dataname
+    if [ -z $dataname ]
+    then
+        dataname=shika
+    fi
+    read -p "Database username [admin]: " username
+    if [ -z $username ]
+    then
+        username=admin
+    fi
+    while [ -z $password ]
+    do
+        read -p "Database password: " password
+    done
     echo -e "DB_DSN=mysql:host=localhost;dbname=$dataname\nDB_USERNAME=$username\nDB_PASSWORD=$password" > .env
-elif [ $database == "2" ]
+elif [ $database == "2" ] #Sqlite
 then
-    echo -e "DB_DSN=sqlite:$path/$dataname.sqlite\nDB_USERNAME=$username\nDB_PASSWORD=$password" > .env
-    if [ -f $dataname.sqlite ]; then
-        echo "Database file already exists at $dataname.sqlite, if this is unexpected you might have to delete it manually and run this script again"
+    echo -e "DB_DSN=sqlite:$path/database.sqlite\nDB_USERNAME=\nDB_PASSWORD=" > .env
+    if [ -f database.sqlite ]; then
+        echo "Database file already exists at database.sqlite, if this is unexpected you might have to delete it manually and run this script again"
     else
-        touch $dataname.sqlite
+        touch database.sqlite
     fi
     echo "Needing sudo password to give permission to database file"
-    sudo chmod 775 $dataname.sqlite
-    sudo chown www-data:www-data $dataname.sqlite
+    sudo chmod 775 database.sqlite
+    sudo chown www-data:www-data database.sqlite
 fi
 
 # Run php script
